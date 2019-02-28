cbead69e9a7a2f66841f876e43412dc05deef06d
Lucas <lucasaugustoneves@hotmail.com>
2019-02-28 11:57:26 -0300

    Adicionar o procfile

--- catalogo_filmes/settings.py
+++ catalogo_filmes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+
+django_heroku.settings(locals())
